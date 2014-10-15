2e5b945af9fc6cfe3426f30d4230b69edd0f681d
Antonio Calanducci <calanducci@etnatraining.it>
2014-10-15 21:35:31 +0200

    updated to Ti SDK 3.4.0.GA

--- Resources/app.js
+++ Resources/app.js
@@ -1,47 +1,56 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('white');
 
-// nota bene, per nascondere la statusBar:
-// su iOS, modificare la property <statusbar-hidden>true</statusbar-hidden> nel file tiapp.xml
-// su Android, modificare la property <statusbar-hidden>true</statusbar-hidden> e <navbar-hidden>true</navbar-hidden> nel file tiapp.xml
 
 // una variante per determinare l'altezza delle finestre in maniera più precisa, anzicchè usare una percentuale approssimata, è quello di sfruttare l'oggetto displayCaps,
 // che mi restituisce l'altezza in pixel del device corrente:
-// var deviceHeight = Ti.Platform.displayCaps.platformHeight;
 
+var deviceHeight = Ti.Platform.displayCaps.platformHeight;
 
-var win1 = Titanium.UI.createWindow({  
-   borderRadius: 5,
-   backgroundColor: "red",
-   left: 10,
-   right: 10,
-   top: 10,
-   height: "47%",
-   //height: (deviceHeight-30)/2,
-   borderWidth: 1,
-   borderColor: "black"
+var win = Ti.UI.createWindow({
+	backgroundColor : "white",
+	fullscreen: true
 });
 
-win1.open();
-
-
-var win2 = Titanium.UI.createWindow({  
-    borderRadius: 5,
-    backgroundColor: "white",
-    backgroundImage: "logo.png",
-    bottom: 10,
-    left: 10,
-    right: 10,
-    height: "47%",
-    //height: (deviceHeight-30)/2,
-    borderWidth: 1,
-    borderColor: "black"
-    
+var view1 = Titanium.UI.createView({
+	borderRadius : 15,
+	backgroundColor : "red",
+	left : 10,
+	right : 10,
+	top : 10,
+	//height : "57%",
+	height: (deviceHeight-30)/2,
+	borderWidth : 1,
+	borderColor : "black"
 });
 
-win2.open();
+win.add(view1);
+
+var view2 = Titanium.UI.createView({
+	borderRadius : 15,
+	backgroundColor : "white",
+	backgroundImage : "logo.png",
+	bottom : 10,
+	left : 10,
+	right : 10,
+	//height : "47%",
+	height: (deviceHeight-30)/2,
+	borderWidth : 1,
+	borderColor : "black"
+
+});
+
+win.add(view2);
+
+win.open();
 
 
+/*win.addEventListener("open", function() {
+	win.activity.actionBar.hide();
+	
+	
+}); */
 
-
+win.addEventListener("postlayout", function() {
+	view1.height = view2.height = win.size.height / 2 - 15;
+	//alert(win.size);
+});
 
